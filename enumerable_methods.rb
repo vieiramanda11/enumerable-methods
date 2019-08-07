19daf75f66d94ce0d0dc790e29667070e624acb2
vieiramanda11 <amanda_vieira@outlook.com>
2019-08-07 17:13:33 -0300

    adding my_each_with_index

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -13,9 +13,22 @@ module Enumerable
     end
     self
   end
+
+  # my_each_with_index method
+  
+  def my_each_with_index
+    i = 0
+    while i < self.length
+      yield(self[i], i)
+      i += 1
+    end
+    self
+  end
 end
 
 # tests
 
-[1, 2, 3].my_each { |x| puts x * 2 }
+#[1, 2, 3].my_each { |x| puts x * 2 }
+
+#[1, 2, 3].my_each_with_index {| i,j| puts i.to_s + " " + j.to_s }
 
