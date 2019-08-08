1a69cb34ed35f54218523dbb546df1816c33f1cc
vieiramanda11 <amanda_vieira@outlook.com>
2019-08-08 12:47:51 -0300

    adding my_select method

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -24,11 +24,25 @@ module Enumerable
     end
     self
   end
+
+  # my_select method
+  def my_select
+    array = Array.new
+    self.my_each do |s|
+      if yield(s)
+        array.push(s)
+      end
+    end
+    array
+  end
+
 end
 
 # tests
+arr = [1, 2, 3, 4, 5]
 
-#[1, 2, 3].my_each { |x| puts x * 2 }
+#arr.my_each { |x| puts x * 2 }
 
-#[1, 2, 3].my_each_with_index {| i,j| puts i.to_s + " " + j.to_s }
+#arr.my_each_with_index {| i,j | puts i.to_s + " " + j.to_s }
 
+arr.my_select { |i| puts i % 2 == 0 }
